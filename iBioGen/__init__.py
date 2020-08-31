b98b8d0599baf703cf0057a01933e774638b8951
Isaac Overcast <isaac.overcast@gmail.com>
2020-08-31 09:14:31 -0400

    "Updating iBioGen/__init__.py to version - 0.0.9

--- iBioGen/__init__.py
+++ iBioGen/__init__.py
@@ -1,7 +1,7 @@
 """
 iBioGen - Phylogeographic Temporal Analysis
 """
-__version__ = "0.0.8"
+__version__ = "0.0.9"
 __author__ = "Isaac Overcast"
 
 ## Possible values for __loglevel__: "DEBUG"  "INFO"  "WARN"  "ERROR"
